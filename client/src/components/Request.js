bcfc5bcf2fa36df958ac183d7860e2609dd59b6d
Colin Henderson <chenderson804@gmail.com>
2018-04-20 19:38:50 -0400

    removed some animation

--- client/src/components/Request.js
+++ client/src/components/Request.js
@@ -9,7 +9,7 @@ const Request = () =>
   <meta http-equiv="X-UA-Compatible" content="ie=edge"></meta>
   <title>Request a Za</title>
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"></link>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.css" />
+  {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.css" /> */}
   <link rel="stylesheet" href="/request.css"></link>
 </head>
 <body>
