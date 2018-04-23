3529b4c61fd00d3a8a91b6d26aa5f75f6ca28fdd
Colin Henderson <chenderson804@gmail.com>
2018-04-22 20:54:28 -0400

    jumbotron change

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -4,8 +4,7 @@ const Jumbotron = ({ children }) => (
   <div
     style={{ height: 100, clear: "both", paddingTop: 50, textAlign: "center", backgroundImage: "url(/images/jumbo.png)",
     backgroundSize: "cover", color: "white", textShadow: "2px 2px black", boxShadow: "5px 10px #888888" }}
-    className="jumbotron"
-  >
+    className="jumbotron">
     {children}
   </div>
 );
